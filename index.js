8b00d57c2ec26e6584ca5481c7b462d0637e7b17
Yash Parmar <yash4parmar12@gmail.com>
2022-10-14 01:00:02 +0530

    -final commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 1000);
+}
+
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
